e304555a6040fb1012d3c48cba7316d7af86b768
veyyr3 <dimapolska967@gmail.com>
2025-06-09 16:27:54 +0600

    created sections

--- Project_CDU/src/App.jsx
+++ Project_CDU/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
+
+// #region компоненты
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// #endregion компоненты
+
+// #region секции
+import MainSection from "./sections/MainSection";
+import MugsSection from "./sections/MugsSection";
+import ProjectSection from "./sections/ProjectSection";
+import GallerySection from "./sections/GallerySection";
+// #endregion секции
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <p>hi!</p>
+            <Footer/>
+        </>
+    )
 }
 
-export default App
+export default App;
