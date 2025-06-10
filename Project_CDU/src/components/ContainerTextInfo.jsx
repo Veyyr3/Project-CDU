46ca48b40b2c8ce83aa42e69b4f942c815cdbbbc
veyyr3 <dimapolska967@gmail.com>
2025-06-10 22:37:54 +0600

    style: changed navs in header and info

--- Project_CDU/src/components/ContainerTextInfo.jsx
+++ Project_CDU/src/components/ContainerTextInfo.jsx
@@ -3,10 +3,12 @@ export default function ContainerTextInfo({titleText, colorfulText, text}) {
     return (
         <div className="container-text-info">
             <div>
-                <h1>{titleText}</h1>
+                <h2>{titleText}</h2>
             </div>
             <div>
-                <p>{`${colorfulText} ${text}`}</p>
+                <p>
+                    <strong>{colorfulText}</strong> {`${text}`}
+                </p>
             </div>
         </div>
     );
