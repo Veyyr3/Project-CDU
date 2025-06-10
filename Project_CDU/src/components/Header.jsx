eee27fdbac0e2b8ecba7584d25514bb680b189cc
veyyr3 <dimapolska967@gmail.com>
2025-06-10 18:39:18 +0600

    header added buttons and style for imgs

--- Project_CDU/src/components/Header.jsx
+++ Project_CDU/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/Images/Logo.png'
+import night_theme_button from "../assets/Images/night_theme_icon.png";
 import Button from './Button'
 
 export default function Header () {
@@ -6,7 +7,7 @@ export default function Header () {
     return (
         <header>
             {/* логотип */}
-            <div id='container-main-logo'>
+            <div id="container-main-logo">
                 <img src={logo} alt="Логотип ЦДЮ" />
             </div>
 
@@ -14,15 +15,18 @@ export default function Header () {
             <nav>
                 <ul>
                     <li>
-                        <Button>Главная</Button>
+                        <Button className="nav_button">Главная</Button>
                     </li>
                     <li>
-                        <Button>Кружки</Button>
+                        <Button className="nav_button">Кружки</Button>
                     </li>
                 </ul>
             </nav>
 
             {/* переключить тему */}
+            <div id="container-night-theme-toggle">
+                <img src={night_theme_button} alt="Переключить тему" />
+            </div>
         </header>
     );
 }
