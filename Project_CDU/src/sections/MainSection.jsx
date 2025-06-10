3867833f2a769a484c09589e736b801c20c5ac73
veyyr3 <dimapolska967@gmail.com>
2025-06-10 23:50:48 +0600

    added imgs for mainSection

--- Project_CDU/src/sections/MainSection.jsx
+++ Project_CDU/src/sections/MainSection.jsx
@@ -1,5 +1,8 @@
+import img1 from '../assets/Images/main_section/main_section_1.jpg';
+import img2 from '../assets/Images/main_section/main_section_2.jpg';
+
 import text_for_container_text_info from "../data.js"; // текст для блоков с информацией
-import ContainerTextInfo from "../components/ContainerTextInfo";
+import ContainerTextInfo from "../components/ContainerTextInfo"; // компонент для текстовой информации
 
 
 export default function MainSection() {
@@ -8,9 +11,22 @@ export default function MainSection() {
     return (
         <section>
             {/* Приветствие */}
-            <ContainerTextInfo key={i[0].id} titleText={i[0].titleText} colorfulText={i[0].colorfulText} text={i[0].text}/>
+            <ContainerTextInfo
+                key={i[0].id}
+                titleText={i[0].titleText}
+                colorfulText={i[0].colorfulText}
+                text={i[0].text}
+            />
 
             {/* блок с фото */}
+            <div className="container-2-imgs">
+                <div>
+                    <img src={img1} alt="" />
+                </div>
+                <div>
+                    <img src={img2} alt="" />
+                </div>
+            </div>
         </section>
     );
 }
