d559ca629ebf373efb62eb98036274db2ec3c6ce
veyyr3 <dimapolska967@gmail.com>
2025-06-11 21:21:58 +0600

    style: flex for ContainerMugsTeacher

--- Project_CDU/src/components/ContainerMugsTeacher.jsx
+++ Project_CDU/src/components/ContainerMugsTeacher.jsx
@@ -4,39 +4,51 @@ export default function ContainerMugsTeacher({imgMugSrc, imgTeacherSrc, titleMug
     return (
         // главный контейнер для фотографий и подписи к ним
         <div className="main-container-imgs-titles">
-            {/* контейнер для кружка и подписи к нему */}
-            <div className="container-imgs-titles-mugs">
+            {/* контейнер для фото кружков и учителей */}
+            <div className="container-imgs">
+
+
                 {/* контейнер для фото кружка */}
-                <div className="container-img">
+                <div className="container-img-mug">
                     <img src={imgMugSrc} alt="" />
                     {/* контейнер для краткого описания фото (изначально он невидим) */}
                     <div className="container-hiden-discription-mug">
                         <p>{discMug}</p>
                     </div>
                 </div>
-                {/* контейнер подписи */}
-                <div className="container-title-imgs">
-                    <h1>{titleMug}</h1>
-                </div>
-            </div>
-            {/* КОНЕЦ контейнер для кружка и подписи к нему */}
 
-            {/* контейнер для учителя и подписи к нему */}
-            <div className="container-imgs-titles-teacher">
+
                 {/* контейнер для фото учителя */}
-                <div className="container-img">
+                <div className="container-img-teacher">
                     <img src={imgTeacherSrc} alt="" />
                     {/* контейнер для краткого описания фото (изначально он невидим) */}
                     <div className="container-hiden-discription-teacher">
                         <p>{discTeacher}</p>
                     </div>
+
                 </div>
-                {/* контейнер подписи */}
-                <div className="container-title-imgs">
-                    <h1>{titleTeacher}</h1>
-                </div>
+
             </div>
-            {/* КОНЕЦ контейнер для учителя и подписи к нему */}
+            {/* КОНЕЦ контейнер для фото кружков и учителей */}
+
+            {/* контейнер для подписей фотографий */}
+            <div className="container-titles-for-imgs">
+
+
+                {/* контейнер подписи кружков */}
+                <div className="container-title-imgs-mug">
+                    <h2>{titleMug}</h2>
+                </div>
+
+
+                {/* контейнер подписи учителей */}
+                <div className="container-title-imgs-teacher">
+                    <h2>{titleTeacher}</h2>
+                </div>
+
+
+            </div>
+            {/* КОНЕЦ контейнер для подписей фотографий */}
         </div>
         // КОНЕЦ главный контейнер для фотографий и подписи к ним
     );
