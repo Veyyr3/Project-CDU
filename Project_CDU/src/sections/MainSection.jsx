128a4b064873809d68cdef26dc08e14045db5efc
veyyr3 <dimapolska967@gmail.com>
2025-06-11 14:46:18 +0600

    style: replace section to main

--- Project_CDU/src/sections/MainSection.jsx
+++ Project_CDU/src/sections/MainSection.jsx
@@ -27,7 +27,7 @@ export default function MainSection() {
 
     return (
         // начало
-        <section>
+        <main>
             {/* Блок Приветствие */}
             <ContainerTextInfo
                 titleText={i[0].titleText}
@@ -57,7 +57,7 @@ export default function MainSection() {
 
             {/* Блок список 2 */}
             <ContainerListInfo titleText={j[1].titleText} list={j[1].list} />
-        </section>
+        </main>
         // конец
     );
 }
