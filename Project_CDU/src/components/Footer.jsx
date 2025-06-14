3733089ff20bf71f481041417ecef8d7f04ab48c
veyyr3 <dimapolska967@gmail.com>
2025-06-14 11:48:50 +0600

    feat: add content Footer

--- Project_CDU/src/components/Footer.jsx
+++ Project_CDU/src/components/Footer.jsx
@@ -1,10 +1,32 @@
-
+// #region компоненты
+    import TwoGisMapWidget from "./TwoGisMapWidget"; // карта для апишки
+// #endregion компоненты
 
 export default function Footer() {
-
     return (
         <footer>
-            <p>Тута футер</p>
+            {/* левая часть */}
+            <address>
+                <h3>Контактная информация:</h3>
+                <p>
+                    <strong>Адрес:</strong>
+                    Улица Сарбагышева, 156 ​Токмок, Чуйский район, Чуйская
+                    область
+                </p>
+                <p>
+                    <strong>Телефон:</strong>
+                    +7 (123) 456-78-90
+                </p>
+                <p>
+                    <strong>Email:</strong>
+                    info@youthcenter.ru
+                </p>
+            </address>
+
+            {/* правая часть */}
+            <div>
+                {/* <TwoGisMapWidget/> */}
+            </div>
         </footer>
-    )
+    );
 }
