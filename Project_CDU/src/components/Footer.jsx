5c59ab892a45d625611a500d8f45b4bbe28d4553
veyyr3 <dimapolska967@gmail.com>
2025-06-14 12:13:52 +0600

    feat: added map to footer

--- Project_CDU/src/components/Footer.jsx
+++ Project_CDU/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 // #region компоненты
-    import TwoGisMapWidget from "./TwoGisMapWidget"; // карта для апишки
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps"; // яндекс карты
 // #endregion компоненты
 
 export default function Footer() {
+    // Координаты для Улица Сарбагышева, 156, Токмок
+    const tokmokCoordinates = [42.847269, 75.299479];
+    const mapZoom = 15; // Можно увеличить масштаб для более детального вида
+
     return (
         <footer>
             {/* левая часть */}
@@ -25,7 +29,33 @@ export default function Footer() {
 
             {/* правая часть */}
             <div>
-                {/* <TwoGisMapWidget/> */}
+                {/* Добавляем modules в YMaps */}
+                <YMaps modules={["geocode", "geoObjects"]}>
+                    {" "}
+                    {/* 'geocode' полезен, 'geoObjects' нужен для Placemark */}
+                    <div>
+                        {/* Убедитесь, что у контейнера карты есть заданная высота */}
+                        <Map
+                            defaultState={{
+                                center: tokmokCoordinates,
+                                zoom: mapZoom,
+                            }}
+                            style={{ width: "100%", height: "400px" }} // Обязательно задайте высоту
+                        >
+                            <Placemark
+                                geometry={tokmokCoordinates}
+                                properties={{
+                                    balloonContent:
+                                        "<strong>Молодежный центр</strong><br/>Улица Сарбагышева, 156", // Текст, который появится при клике на метку
+                                    hintContent: "Наш адрес в Токмоке", // Текст, который появится при наведении на метку
+                                }}
+                                options={{
+                                    preset: "islands#blueDotIcon",
+                                }}
+                            />
+                        </Map>
+                    </div>
+                </YMaps>
             </div>
         </footer>
     );
